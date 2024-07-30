f3dfd729a10ad114bbd8c7ce4821f7e5e5b3108a
Mirko Abozzi <mirko.abozzi@gmail.com>
2024-07-30 14:56:36 +0200

    es.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import java.util.HashSet;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        Set<String> listaParole = new HashSet<>();
+
+        int nElementi = 0;
+
+        System.out.println("Quanti elementi vuoi inserire nela Collezione?");
+        try {
+            nElementi = scanner.nextInt();
+            scanner.nextLine();
+        } catch (InputMismatchException ex) {
+            System.out.println("Digita un numero");
+        }
+
+        for (int i = 0; i < nElementi; i++) {
+            System.out.println("Inserisci parola");
+            String parola = scanner.nextLine();
+
+            if (listaParole.contains(parola)) {
+                System.out.println("Duplicato: " + parola);
+                i--;
+            } else {
+                listaParole.add(parola);
+            }
+        }
+
+        System.out.println(listaParole);
+
     }
 }
